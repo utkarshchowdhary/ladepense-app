b3eb15427bdd78663fd41db232fada4b1d65be03
Utkarsh Chowdhary <utkarshchowdhary4140@gmail.com>
2020-09-02 22:18:42 +0530

    refactored env path

--- src/tests/setupTests.js
+++ src/tests/setupTests.js
@@ -3,7 +3,7 @@ import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import Dotenv from 'dotenv';
 
-Dotenv.config({ path: '.env.test' });
+Dotenv.config({ path: 'config/test.env' });
 Enzyme.configure({
   adapter: new Adapter(),
 });
--- webpack.config.js
+++ webpack.config.js
@@ -4,10 +4,8 @@ const webpack = require('webpack');
 
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
-if (process.env.NODE_ENV === 'test') {
-  require('dotenv').config({ path: '.env.test' });
-} else if (process.env.NODE_ENV === 'development') {
-  require('dotenv').config({ path: '.env.development' });
+if (process.env.NODE_ENV === 'development') {
+  require('dotenv').config({ path: 'config/dev.env' });
 }
 
 module.exports = (env) => {
