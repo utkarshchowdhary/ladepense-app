9800e9dcb50b42276536f83e997a8536649bfd07
Utkarsh Chowdhary <utkarshchowdhary4140@gmail.com>
2021-05-28 12:11:45 +0530

    removed console log

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -2,20 +2,17 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { startLogin } from '../actions/auth'
 
-export const LoginPage = props => {
-  console.log(props)
-  return (
-    <div className='box-layout'>
-      <div className='box-layout__box'>
-        <h1 className='box-layout__title'>Ladépense App</h1>
-        <p>Whether it’s cloudy or sunny, you should be saving money.</p>
-        <button className='button' onClick={props.startLogin}>
-          Login with Google
-        </button>
-      </div>
+export const LoginPage = ({ startLogin }) => (
+  <div className='box-layout'>
+    <div className='box-layout__box'>
+      <h1 className='box-layout__title'>Ladépense App</h1>
+      <p>Whether it’s cloudy or sunny, you should be saving money.</p>
+      <button className='button' onClick={startLogin}>
+        Login with Google
+      </button>
     </div>
-  )
-}
+  </div>
+)
 
 const mapDispatchToProps = dispatch => ({
   startLogin: () => dispatch(startLogin())
